67754463b3b8a3612d3d99aab2a07d6ac864bac8
Eric Butera <eric@ericbutera.com>
2022-03-01 20:24:42 -0500

    equalize array

---
+++ app/src/main/kotlin/code/challenges/EqualizeArray.kt
@@ -0,0 +1,38 @@
+// https://www.hackerrank.com/challenges/equality-in-a-array/problem
+package code.challenges
+
+fun equalizeArray(arr: Array<Int>): Int {
+    val counts = HashMap<Int, Int>()
+    var max = 0 // contains the highest found count across arr
+
+    for (number in arr) {
+        val attempt = (counts[number] ?: 0) + 1
+        counts[number] = attempt
+
+        if (attempt > max)
+            max = attempt
+    }
+
+    // leave only elements of equal value.
+    // if counts.keys.size == 1, return 0
+
+    // calculate to remove:
+    // SUM all "less than max" values
+    // this fails on 1,1,1,2,2,2.. must delete 1 or 2
+    // add maxUsed so that only the first "max" count entry skips. the rest are summed
+    var removeCount = 0
+    var maxUsed = false
+    for ((_, count) in counts) {
+        if (!maxUsed && count == max) {
+            maxUsed = true
+            continue;
+        }
+
+        removeCount += count
+    }
+
+    return removeCount
+
+    // found this in discussion. nice optimization that takes total length - highest count using subtraction property
+    //return arr.size - max
+}
---
+++ app/src/test/kotlin/code/challenges/EqualizeArrayTest.kt
@@ -0,0 +1,39 @@
+package code.challenges
+
+import kotlin.test.*
+
+class EqualizeArrayTest {
+    @Test fun sample() {
+        assertEquals(2, equalizeArray(arrayOf(3, 3, 2, 1, 3)))
+    }
+
+    @Test fun case17() {
+        assertEquals(4, equalizeArray(arrayOf(1,2,3,1,2,3,3,3)))
+        /*
+        11
+        22
+        3333
+
+         */
+    }
+
+    @Test fun case3() {
+        println(equalizeArray(arrayOf(1,1,1,2,2,2)))
+    }
+
+    @Test fun case4() {
+        println(equalizeArray(arrayOf(1,1,2,2,2)))
+    }
+
+    @Test fun case5() {
+        println(equalizeArray(arrayOf(1,2,2,2)))
+    }
+
+    @Test fun case6() {
+        println(equalizeArray(arrayOf(1,1,1,2,2,2,3,3,3)))
+    }
+
+    @Test fun case7() {
+        println(equalizeArray(arrayOf(1,1,2,2)))
+    }
+}
